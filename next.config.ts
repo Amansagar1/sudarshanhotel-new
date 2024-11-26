20caabf2ef434d13cfce77a318d859ae1b9aa62e
sagar sharma <sagarsharmaofficial0@gmail.com>
2024-11-26 10:23:17 +0530

    build done

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-
+  distDir: "build",
+  output: "standalone",
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   webpack(config, { isServer }) {
     if (!isServer) {
       config.module.rules.push({
