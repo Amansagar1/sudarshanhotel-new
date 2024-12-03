38576764547c1cf1fe7f7abfa1bf43a11765ad5e
sagar sharma <sagarsharmaofficial0@gmail.com>
2024-12-03 22:52:12 +0530

    basepath added

--- build/server/middleware-build-manifest.js
+++ build/server/middleware-build-manifest.js
@@ -1,1 +1,19 @@
-self.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-a629f7f3a5517537.js","static/chunks/4bd1b696-d153e37807b247ce.js","static/chunks/517-82477a57439e21c9.js","static/chunks/main-app-3ff733d58873c6a1.js"],rootMainFilesTree:{},pages:{"/_app":["static/chunks/webpack-a629f7f3a5517537.js","static/chunks/framework-881f0560c40ae817.js","static/chunks/main-22fab05f9169b18d.js","static/chunks/pages/_app-d23763e3e6c904ff.js"],"/_error":["static/chunks/webpack-a629f7f3a5517537.js","static/chunks/framework-881f0560c40ae817.js","static/chunks/main-22fab05f9169b18d.js","static/chunks/pages/_error-9b7125ad1a1e68fa.js"]},ampFirstPages:[]},self.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+self.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [],
+  "rootMainFilesTree": {},
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+self.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
